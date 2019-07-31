0cc433fce088fe5e892afa03c2be7ce676982382
simon-bee <simonbee1303@gmail.com>
2019-07-31 10:21:54 +0200

    menu mobile admin

--- www/public/assets/js/script.js
+++ www/public/assets/js/script.js
@@ -31,3 +31,16 @@ function deleting(path, id) {
 		})
 	}
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var panel = this.nextElementSibling;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		/* Toggle between adding and removing the "active" class,
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+		document.getElementById('panel').classList.toggle("block");
+	});
+}
